9c1dff1d17dacc8383d7ff96f88cd905bf6ba658
@revakovsky <revakovsky@gmail.com>
2022-12-13 22:05:20 +0200

    refactor project

--- app/src/main/java/com/revakovskyi/catchcoins/utils/SharedPrefs.kt
+++ app/src/main/java/com/revakovskyi/catchcoins/utils/SharedPrefs.kt
@@ -7,32 +7,22 @@ import androidx.core.content.edit
 class SharedPrefs(context: Context) {
     private val shPrefs = context.getSharedPreferences(SHARED_PREFS_KEY, MODE_PRIVATE)
 
-    fun saveCurrentScore(currentScore: Int) {
-        shPrefs.edit { putInt(SP_CURRENT_SCORE, currentScore) }
-    }
+    fun saveCurrentScore(currentScore: Int) { shPrefs.edit { putInt(SP_CURRENT_SCORE, currentScore) }}
 
-    fun getCurrentScore(): Int {
-        return shPrefs.getInt(SP_CURRENT_SCORE, 0)
-    }
+    fun getCurrentScore(): Int = shPrefs.getInt(SP_CURRENT_SCORE, 0)
 
-    fun clearCurrentScore() {
-        shPrefs.edit { remove(SP_CURRENT_SCORE) }
-    }
+    fun clearCurrentScore() { shPrefs.edit { remove(SP_CURRENT_SCORE) }}
 
     fun saveMaxScore(score: Int) {
         val maxScore = getMaxScore()
-
         if (score > maxScore)
             shPrefs.edit { putInt(SP_MAX_SCORE, score) }
     }
 
-    fun getMaxScore(): Int {
-        return shPrefs.getInt(SP_MAX_SCORE, 0)
-    }
+    fun getMaxScore(): Int = shPrefs.getInt(SP_MAX_SCORE, 0)
 
-    fun clearMaxScore() {
-        shPrefs.edit { remove(SP_MAX_SCORE) }
-    }
+    fun clearMaxScore() { shPrefs.edit { remove(SP_MAX_SCORE) }}
+
 
     companion object {
         private const val SHARED_PREFS_KEY = "KEY"
