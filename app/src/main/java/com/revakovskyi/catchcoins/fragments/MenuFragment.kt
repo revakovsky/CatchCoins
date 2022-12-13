570b4e89ecb86362e9e5255d9b2ecd8b08aefed8
@revakovsky <revakovsky@gmail.com>
2022-12-13 12:33:26 +0200

    add shared preferences, add Best Result dashboard

--- app/src/main/java/com/revakovskyi/catchcoins/fragments/MenuFragment.kt
+++ app/src/main/java/com/revakovskyi/catchcoins/fragments/MenuFragment.kt
@@ -1,33 +1,87 @@
 package com.revakovskyi.catchcoins.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.revakovskyi.catchcoins.R
 import com.revakovskyi.catchcoins.databinding.FragmentMenuBinding
+import com.revakovskyi.catchcoins.utils.SharedPrefs
 import com.revakovskyi.catchcoins.utils.showExitDialog
 
 class MenuFragment : Fragment(R.layout.fragment_menu) {
 
-    private lateinit var binding: FragmentMenuBinding
+    private var binding: FragmentMenuBinding? = null
+    private var sharedPrefs: SharedPrefs? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMenuBinding.bind(view)
+        sharedPrefs = SharedPrefs(requireActivity())
 
-        binding.startGameButton.setOnClickListener {
+        binding?.startGameButton?.setOnClickListener {
             findNavController().navigate(R.id.action_menuFragment2_to_gameFragment2)
         }
 
-        binding.bestResultButton.setOnClickListener {
-            //todo
+        binding?.bestResultButton?.setOnClickListener {
+            openDashboard()
         }
 
-        binding.exitButton.setOnClickListener {
+        binding?.exitButton?.setOnClickListener {
             showExitDialog()
         }
 
+    }
+
+    private fun openDashboard() {
+        changeMenuVisibility(View.GONE)
+
+        binding?.apply {
+            coinCounter.text = sharedPrefs?.getMaxScore().toString()
+            clearButton.setOnClickListener {
+                showIntentDialog()
+            }
+            closeDashboardButton.setOnClickListener {
+                changeMenuVisibility(View.VISIBLE)
+            }
+        }
+    }
+
+    private fun changeMenuVisibility(visibilityValue: Int) {
+        binding?.apply {
+            dowerChestImage.visibility = visibilityValue
+            coinRainImage.visibility = visibilityValue
+            imageView.visibility = visibilityValue
+            gameTitle.visibility = visibilityValue
+            startGameButton.visibility = visibilityValue
+            bestResultButton.visibility = visibilityValue
+            exitButton.visibility = visibilityValue
+
+            when (visibilityValue) {
+                View.GONE -> dashboard.visibility = View.VISIBLE
+                View.VISIBLE -> dashboard.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun showIntentDialog() {
+        AlertDialog.Builder(requireContext())
+            .setIcon(R.drawable.question_icon)
+            .setTitle(R.string.clear)
+            .setMessage(R.string.are_you_sure)
+            .setPositiveButton(R.string.yes) { _, _ ->
+                sharedPrefs?.clearMaxScore()
+            }
+            .setNegativeButton(R.string.no, null)
+            .create()
+            .show()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        binding = null
+        sharedPrefs = null
     }
 
 }
---
+++ app/src/main/java/com/revakovskyi/catchcoins/utils/SharedPrefs.kt
@@ -0,0 +1,38 @@
+package com.revakovskyi.catchcoins.utils
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import androidx.core.content.edit
+
+class SharedPrefs(context: Context) {
+    private val shPrefs = context.getSharedPreferences(SHARED_PREFS_KEY, MODE_PRIVATE)
+
+    fun saveCurrentScore(currentScore: Int) {
+        shPrefs.edit { putInt(SP_CURRENT_SCORE, currentScore) }
+    }
+
+    fun getCurrentScore(): Int {
+        return shPrefs.getInt(SP_CURRENT_SCORE, 0)
+    }
+
+    fun saveMaxScore(score: Int) {
+        val maxScore = getMaxScore()
+
+        if (score > maxScore)
+            shPrefs.edit { putInt(SP_MAX_SCORE, score) }
+    }
+
+    fun getMaxScore(): Int {
+        return shPrefs.getInt(SP_MAX_SCORE, 0)
+    }
+
+    fun clearMaxScore() {
+        shPrefs.edit { remove(SP_MAX_SCORE) }
+    }
+
+    companion object {
+        private const val SHARED_PREFS_KEY = "KEY"
+        private const val SP_CURRENT_SCORE = "CURRENT"
+        private const val SP_MAX_SCORE = "MAX"
+    }
+}
